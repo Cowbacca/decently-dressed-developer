2c0450856a504d2f58de06145a0e073c3a1e71e2
Angus J Goldsmith <angus@dashery.co.uk>
2017-12-23 21:26:27 +0000

    Added Contentful attribution footer.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -18,6 +18,19 @@ const Header = () => (
   </div>
 )
 
+const Footer = () => (
+  <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
+    <img
+      src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
+      style={{
+        'max-width': '100px',
+        width: '100%'
+      }}
+      alt="Powered by Contentful"
+    />
+  </a>
+)
+
 interface GatsbyLayoutProps {
   children: () => any
 }
@@ -38,6 +51,7 @@ const TemplateWrapper = ({children}: GatsbyLayoutProps) => (
     <div>
       {children()}
     </div>
+    <Footer/>
   </div>
 )
 
