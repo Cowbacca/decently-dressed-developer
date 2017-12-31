507542529058181b9895cc53eace9059d0eb0c7f
Angus J Goldsmith <angus@dashery.co.uk>
2017-12-31 00:00:40 +0000

    Style elements in footer now have reacty property names.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -18,12 +18,12 @@ const Header = () => (
 )
 
 const Footer = () => (
-  <footer style={{'padding-top': '1em'}}>
+  <footer style={{paddingTop: '1em'}}>
       <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
         <img
           src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
           style={{
-            'max-width': '100px',
+            maxWidth: '100px',
             width: '100%'
           }}
           alt="Powered by Contentful"
