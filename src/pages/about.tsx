02c13f4fb4cfad5347ff81e71d7b5b3027d43a63
Angus J Goldsmith <angus@dashery.co.uk>
2018-01-01 17:19:58 +0000

    Added place holder about page.

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default () => (
+  <article>
+    <h2>About</h2>
+    <p>Here lies the about section of the Decently Dressed Developer website.</p>
+    <p>It is not yet complete.</p>
+  </article>
+)
