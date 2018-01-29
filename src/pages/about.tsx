1536af6eaacdb43879aca2d1450a80d8d1666d9c
Angus Goldsmith <durins_beard@hotmail.com>
2018-01-29 20:20:24 +0000

    Improve about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,13 @@ import * as React from 'react'
 export default () => (
   <article>
     <h2>About</h2>
-    <p>Here lies the about section of the Decently Dressed Developer website.</p>
-    <p>It is not yet complete.</p>
+    <h3>The Website</h3>
+    <p>The Decently Dressed Developer was the result of a combined assault. On the positive front: colleagues and associates expressing a desire for guidance in upping their attire
+      game, if only just a little. Meanwhile, a subset of my fellow nerds charged my sensory flanks with their earnest yet unfortunate attempts at dandification. I hope this
+      website can be of some help to either type of person; I have certainly been in both camps myself!</p>
+    <p>If you have any feature requests or suggestions, please feel free to raise a GitHub issue.  If you're feeling really adventurous, you could even raise a pull request.</p>
+    <h3>Me</h3>
+    <p>I'm a pragmatic software developer with a focus on design and architecture.  In my free time I like to knit, make music, and play boardgames.</p>
+    <p>You can find me on <a href="https://www.linkedin.com/in/angus-goldsmith-1567b3105/">Linked-In</a> and <a href="https://github.com/Cowbacca">GitHub</a>.</p>
   </article>
 )
